bd096d2df3fb4c3da5707446b0227b7d2dda2315
Jhunel B Penaflorida <jhunelpenaflorida@gmail.com>
2023-01-17 14:12:19 +0800

    Added the addUser API.

--- src/main/java/com/example/Ecommerce/Controller/UserController.java
+++ src/main/java/com/example/Ecommerce/Controller/UserController.java
@@ -1,7 +1,23 @@
 package com.example.Ecommerce.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Ecommerce.Entity.UserEntity;
+import com.example.Ecommerce.Repository.UserRepository;
+import com.example.Ecommerce.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    //Add the User Registration
+    @PostMapping(path = "/adduser")
+    public UserEntity addUser(@RequestParam String email, String password, String role){
+        return userService.addUser(email,password,role);
+    }
+
 }
--- src/main/java/com/example/Ecommerce/Service/UserService.java
+++ src/main/java/com/example/Ecommerce/Service/UserService.java
@@ -1,9 +1,21 @@
 package com.example.Ecommerce.Service;
 
+import com.example.Ecommerce.Entity.UserEntity;
+import com.example.Ecommerce.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    UserRepository userRepository;
 
+    public UserEntity addUser(String email, String password, String role) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(email);
+        userEntity.setPassword(password);
+        userEntity.setRole(role);
+        return userRepository.save(userEntity);
+    }
 }
