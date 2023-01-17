3e4c4d60bb6f4ad6567ef55cb4086b934d95aeca
Jhunel B Penaflorida <jhunelpenaflorida@gmail.com>
2023-01-17 13:34:46 +0800

    Edited the UserEntity under Entity Package.

--- src/main/java/com/example/Ecommerce/Entity/UserEntity.java
+++ src/main/java/com/example/Ecommerce/Entity/UserEntity.java
@@ -1,4 +1,12 @@
 package com.example.Ecommerce.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class UserEntity {
+    @Id
+
 }
