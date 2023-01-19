200a523abd36158e706034b7aa5482039dc2265a
Joash Dellota <joash.dellota@novare.com.hk>
2023-01-19 18:02:25 +0800

    Add product ap v2

---
+++ src/main/java/com/example/Ecommerce/dto/ProductDto.java
@@ -0,0 +1,31 @@
+package com.example.Ecommerce.dto;
+
+public class ProductDto {
+    private String name;
+    private float price;
+    private int userid;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+}
